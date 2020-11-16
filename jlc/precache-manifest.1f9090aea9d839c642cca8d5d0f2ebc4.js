f231ef62df74ac379609881bcf7a853f952f784b
HWR0 <49004465+hwreverse@users.noreply.github.com>
2020-11-16 21:58:07 +0300

    Added jlcparts subfolder

---
+++ jlc/precache-manifest.1f9090aea9d839c642cca8d5d0f2ebc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df496d3ff81878bf4cbe3a96e73e8c0f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9676c804b320af13dac3",
+    "url": "./static/css/main.9b6ca35a.chunk.css"
+  },
+  {
+    "revision": "b5bfbb2ba3e98b563f6e",
+    "url": "./static/js/2.9241ecc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9241ecc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9676c804b320af13dac3",
+    "url": "./static/js/main.f1c6f8e5.chunk.js"
+  },
+  {
+    "revision": "1093f95fb2ed020f250e",
+    "url": "./static/js/runtime-main.3fc2b595.js"
+  }
+]);
